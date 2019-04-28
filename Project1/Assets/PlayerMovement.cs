c2bf716f46a2cc5454f20cb39570f77f457c5de7
mariuskv <50083526+mariuskv@users.noreply.github.com>
2019-04-28 22:14:51 +0200

    Added movement to Player

---
+++ Project1/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+    public float movementSpeed = 40f;
+    float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
